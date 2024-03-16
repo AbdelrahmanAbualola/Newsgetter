5231928deea875707acd2faacc561e4d7f6f5f46
AbdelrahmanAbualola <115497203+AbdelrahmanAbualola@users.noreply.github.com>
2024-03-16 22:31:35 +0200

    Upgrade: Add 'Newsletters.py'

---
+++ Newsletters.py
@@ -0,0 +1,19 @@
+class Newsletters:
+    def Newsletters_Parameters():
+        Newsletters_Parameters = {
+            'ESG Newsletter':{
+                            'Keywords':['ESG', 'renewable energy', 'climate change', 'green hydrogen', 'clean energy', 'environment', 'emissions', 'net zero', 'carbon capture', 'governance', 'gender', 'inclusion', 'diversity'],
+                            'Countries':['Saudi Arabia', 'United Arab Emirates', 'Oman', 'Qatar', 'Kuwait', 'Bahrain', 'Egypt']
+                            },
+            
+            'Localization Newsletter':{
+                            'Keywords':["Localization", "localisation", "Local content", "in country value", "localize" , "localise" , "technology transfer"],
+                            'Countries':['Saudi Arabia', 'United Arab Emirates', 'Oman', 'Qatar', 'Kuwait', 'Bahrain', 'Egypt', 'Malaysia', 'Indonesia', 'Singapore', 'China', 'India', 'Australia', 'Canada']
+                            }
+            
+            
+        }
+        
+        return Newsletters_Parameters
+
+
